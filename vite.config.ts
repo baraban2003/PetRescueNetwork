eaa4f910ed5f8b7f321a194097245f149f232bbc
Volodymyr Baraban <barabanv2003@gmail.com>
2024-01-30 15:52:18 +0200

    Sim git hub fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
-import svgr from 'vite-plugin-svgr'
+import svgr from "vite-plugin-svgr"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/your repo/",
-  plugins: [
-    react(),
-    svgr(),
-
-  ],
+  base: "/PetRescueNetwork/",
+  plugins: [react(), svgr()],
   server: {
     open: true,
   },
