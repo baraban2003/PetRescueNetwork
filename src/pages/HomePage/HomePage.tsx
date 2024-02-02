d3d3ea6991b8723deebcfbae014254c8201a6005
Volodymyr Baraban <barabanv2003@gmail.com>
2024-02-02 23:33:09 +0200

    Add

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -5,7 +5,6 @@ export function HomePage() {
         <h1>Home Page</h1>
         <p>Welcome to the best resource for saving contacts you have.</p>
         <p>For using this application please Register.</p>
-        <p>If you are a registered user please Sign in.</p>
       </section>
     </>
   )
