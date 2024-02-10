f8307d08704a73ae6cffae362db6d2d5b8701e6d
Volodymyr Baraban <barabanv2003@gmail.com>
2024-02-10 18:20:54 +0200

    home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,15 +4,13 @@ export function HomePage() {
   return (
     <>
       <section className={s.homePage}>
-        <div className={s.homePage__picture}></div>
-        <div className={s.homePage__block}>
-          <h1 className={s.homePage__text}>
-            Extend a Paw <br /> Share the Care
-          </h1>
-          <h2 className={s.homePage__text}>
-            Support Our <br /> Fluffy Friends <br /> in Need.
-          </h2>
-        </div>
+        <h1 className={s.homePage__text}>
+          Extend a Paw <br /> Share the Care.
+        </h1>
+
+        <h2 className={s.homePage__text}>
+          Support Fluffy <br /> Friends in Need.
+        </h2>
       </section>
     </>
   )
